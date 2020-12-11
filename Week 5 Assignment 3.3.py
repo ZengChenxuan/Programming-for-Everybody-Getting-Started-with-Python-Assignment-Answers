501b3b451b67981a9419f71f1babcd8203f0c1ef
ZengChenxuan <75615737+ZengChenxuan@users.noreply.github.com>
2020-12-11 18:24:27 +0800

    Update Week 5 Assignment 3.3.py

--- Week 5 Assignment 3.3.py
+++ Week 5 Assignment 3.3.py
@@ -1,14 +1,18 @@
 score = input("Enter Score: ")
-score1 = float(score)
+try:
+    score1 = float(score)
+except:
+    print('ERROR: Plz enter numerical input\nsystem exiting...')
+    quit()
 if score1>=0.9 :
     print('A')
-elif score1<0.9 and score1>=0.8 :
+elif score1>=0.8 :
     print('B')
-elif score1<0.8 and score1>=0.7 :
+elif score1>=0.7 :
     print('C')
-elif score1<0.7 and score1>=0.6 :
+elif score1>=0.6 :
     print('D')
-elif score1<0.6 :
+elif score1>=0 :
     print('F') 
 else:
-    print('ERROR')
+    print('ERROR: Plz enter a non-negative score')
